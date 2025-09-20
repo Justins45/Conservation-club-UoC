4fce3bb844a061bed37b7cc9fb31033495cdcb5e
Justin Shaw <shaw.justin@shaw.ca>
2025-09-20 13:55:38 -0600

    init about-template and fix footer current page highlighting

---
+++ about-template.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: About Template
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+  <div>
+
+    <h1>About The Club</h1>
+
+  </div>
+
+	</main>
+
+<?php
+get_sidebar();
+get_footer();
