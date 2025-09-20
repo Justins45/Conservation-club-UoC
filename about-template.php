eb1b71c1ee41f8986709f8f45066c5783202199b
Justin Shaw <shaw.justin@shaw.ca>
2025-09-20 15:35:27 -0600

    init about us page

--- about-template.php
+++ about-template.php
@@ -10,8 +10,46 @@ get_header();
 
   <div>
 
-    <h1>About The Club</h1>
+    <div>
+      <!-- image -->
+      <h1>About The Club</h1>
+    </div>
+    <div>
+      <p>Welcome to the Wildlife Conservation Club at the University of Calgary! We are a passionate, student-led community committed to protecting wildlife and promoting sustainable, eco-friendly living both on campus and beyond.</p>
+      <p>This website is our digital home; a space for awareness, creativity, and action. Here, you can:</p>
 
+      <div>
+        <div>
+          <h2>Submit Your Own Work</h2>
+          <p>Poems, opinion pieces, research papers, and artwork on wildlife, conservation, climate, and environmental justice.</p>
+        </div>
+        <div>
+        <h2>Explore Student Contributions</h2>  
+          <p>
+            Explore student contributions: Read original writing and research from fellow students passionate about biodiversity and sustainability.
+          </p>
+        </div>
+        <div>
+        <h2>Stay Up To Date</h2>  
+          <p>
+            Find information on upcoming events, volunteer opportunities, and conservation campaigns.
+          </p>
+        </div>
+        <div>
+        <h2>Get Involved</h2>  
+          <p>
+            Learn how to join our club, collaborate on initiatives, or take part in advocacy and community outreach.
+          </p>
+        </div>
+        <div>
+        <h2>Access resources</h2>  
+          <p>
+            Discover helpful links, reading lists, and tools for becoming a more informed and active conservationist.
+          </p>
+        </div>
+      </div>
+      <p>Whether you're a writer, researcher, activist, or simply care deeply about the planet, your voice matters here. Let's work together to inspire change: one idea, one action, and one story at a time.</p>
+    </div>
   </div>
 
 	</main>
