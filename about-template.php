26d7c5e1e5ea9b3514129fcba3e4860cb4db8617
Justin Shaw <shaw.justin@shaw.ca>
2025-09-21 00:11:11 -0600

    add desktop styling to about us content

--- about-template.php
+++ about-template.php
@@ -20,36 +20,41 @@ get_header();
         <p>This website is our digital home; a space for awareness, creativity, and action. Here, you can:</p>
       </div>
       
-      <div>
-        <div class="about-container about-card about-card-lb">
-          <h2>Submit Your Own Work</h2>
-          <p>Poems, opinion pieces, research papers, and artwork on wildlife, conservation, climate, and environmental justice.</p>
+      <div class="about-card-container">
+        <div class="about-card-grid-top">
+          <div class="about-container about-card about-card-lb">
+            <h2>Submit Your Own Work</h2>
+            <p>Poems, opinion pieces, research papers, and artwork on wildlife, conservation, climate, and environmental justice.</p>
+          </div>
+          <div class="about-container about-card about-card-br">
+            <h2>Explore Student Contributions</h2>  
+            <p>
+              Explore student contributions: Read original writing and research from fellow students passionate about biodiversity and sustainability.
+            </p>
+          </div>
+          <div class="about-container about-card about-card-or">
+            <h2>Stay Up To Date</h2>  
+            <p>
+              Find information on upcoming events, volunteer opportunities, and conservation campaigns.
+            </p>
+          </div>
         </div>
-        <div class="about-container about-card about-card-br">
-          <h2>Explore Student Contributions</h2>  
-          <p>
-            Explore student contributions: Read original writing and research from fellow students passionate about biodiversity and sustainability.
-          </p>
-        </div>
-        <div class="about-container about-card about-card-or">
-          <h2>Stay Up To Date</h2>  
-          <p>
-            Find information on upcoming events, volunteer opportunities, and conservation campaigns.
-          </p>
-        </div>
-        <div class="about-container about-card about-card-lg">
-          <h2>Get Involved</h2>  
-          <p>
-            Learn how to join our club, collaborate on initiatives, or take part in advocacy and community outreach.
-          </p>
-        </div>
-        <div class="about-container about-card about-card-dg">
-          <h2>Access resources</h2>  
-          <p>
-            Discover helpful links, reading lists, and tools for becoming a more informed and active conservationist.
-          </p>
+        <div class="about-card-grid-bottom">
+          <div class="about-container about-card about-card-lg">
+            <h2>Get Involved</h2>  
+            <p>
+              Learn how to join our club, collaborate on initiatives, or take part in advocacy and community outreach.
+            </p>
+          </div>
+          <div class="about-container about-card about-card-dg">
+            <h2>Access resources</h2>  
+            <p>
+              Discover helpful links, reading lists, and tools for becoming a more informed and active conservationist.
+            </p>
+          </div>
         </div>
       </div>
+      
       <div class="about-container">
         <p>Whether you're a writer, researcher, activist, or simply care deeply about the planet, your voice matters here. Let's work together to inspire change: one idea, one action, and one story at a time.</p>
       </div>
