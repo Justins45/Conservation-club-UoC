62f226cec2661133b8f467e3ec7836b304b5717c
Justin Shaw <shaw.justin@shaw.ca>
2025-10-03 18:12:37 -0600

    add get involved page

---
+++ get-involved-template.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: Get Involved Template
+ */
+
+get_header();
+?>
+
+<main id="primary" class="site-main">
+  <h1>Get Involved</h1>
+
+  <div class="content-container">
+    <div class="bake-sale">
+      <h2>Bake Sales Donations</h2>
+      <p>For each bake sale we donate a percentage of the profits to different wildlife societies </p>
+    </div>
+    <div class="get-involved-completed">
+      <h2>So far we have done:</h2>
+      <ul>
+        <li><a href="https://www.calgarywildlife.org/" target="_blank">Calgary Wildlife Rehabilitation Society</a></li>
+        <li><a href="https://www.aiwc.ca/" target="_blank">Alberta Institute For Wildlife Conservation</a></li>
+        <li><a href="https://burrowingowl.com/content.php?p=home">Alberta Birds of Prey Foundation</a></li>
+      </ul>
+    </div>
+  </div>
+</main>
+
+<?php
+get_sidebar();
+get_footer();
