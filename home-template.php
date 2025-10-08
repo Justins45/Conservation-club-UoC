3a3d28ae4256641238242e4850cac776677d2f50
Justin Shaw <shaw.justin@shaw.ca>
2025-10-07 20:35:42 -0600

    add more content to home page

--- home-template.php
+++ home-template.php
@@ -6,43 +6,61 @@
 get_header();
 ?>
 
-<main id="primary" class="site-main">
+<main id="primary" class="">
   <div>
     <h1>Wildlife Conservation Club</h1>
-      <p>Investing in the Future of Our Planet</p>
-    </div>
-  <div>  
-    <div class="content-container multi-intro">
+    <p>Investing in the Future of Our Planet</p>
+  </div>
+  <div class="site-main content-container">  
+    <div class=" multi-intro">
       <h2>Why is this Important? Who are we?</h2>
       <h3>What is the WWC?</h3>
       <p>The WCC is a University of Calgary SU Club that aims to conserve and protect the habitats of endangered species while also raising awareness about sustainability and sustainable practices.</p>
       <p>Important especially now during times where many animals are endangered and in a time of ecological crisis.</p>
     </div>
     <div class="home-posts">
-      <?php
-        if ( have_posts() ) :
-          /* Start the Loop */
-          while ( have_posts() ) :
-            the_post();
-            /*
-            * Include the Post-Type-specific template for the content.
-            * If you want to override this in a child theme, then include a file
-            * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-            */
-            get_template_part( 'template-parts/content', get_post_type() );
-
-          endwhile;
-
-          the_posts_navigation();
-
-        else :
-
-          get_template_part( 'template-parts/content', 'none' );
-
-        endif;
+      <h2>Latest Posts</h2>
+      <?php 
+        $args = array(
+          'post_type'=> 'post',
+          'orderby'    => 'ID',
+          'post_status' => 'publish',
+          'order'    => 'DESC',
+          'posts_per_page' => 5
+        );
+        $result = new WP_Query( $args );
+        if ( $result-> have_posts() ) : 
       ?>
+      <div class="home-submissions-container">
+        <?php while ( $result->have_posts() ) : $result->the_post(); ?>
+          <div class="home-submission-block">
+            <h2 class="submission-title"><?php the_title(); ?></h2>
+            <p class="submission-excerpt"><?php the_excerpt(); ?></p>
+            <p class="submission-author"><?php the_author(); ?> | <?php the_date(); ?></p>
+            <p></p>
+
+            <a href="<?php the_permalink(); ?>" target="_blank">Read more!</a>
+          </div>
+        <?php endwhile; ?>
+      <?php endif;?>
+      </div>
+    </div>
+    <div class="be-the-difference-container">
+    <div class="be-the-difference">
+          <h2>Be the Difference</h2>
+          <p>There are a multitude of ways you can be the difference. It all starts with an action, starting small always and sticking to your goals will always lead to a bigger impact.</p>
+          <p>Check out our Get Involved page to learn more details about how you can be the Difference</p>
+    </div>
+    <div>
+      <!-- image -->
     </div>
   </div>
+  <div class="why-needed">
+    <h2>Why is this needed?</h2>
+    <p>Animal conservation is <span class="bolded">crucial</span> not only for the survival of individual species but also for the health of <span class="bolded">entire ecosystems and human well-being</span>. Each species plays a role in the intricate balance of life, and its disappearance can have widespread and unpredictable effects.</p>
+  </div>
+  </div>
+  
 </main>
 
 <?php
