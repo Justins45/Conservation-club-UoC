b8babe46fd86caae6a59580a4eb130dc3404c8a6
Justin Shaw <shaw.justin@shaw.ca>
2026-01-01 13:30:49 -0700

    added 404 page styles

--- 404.php
+++ 404.php
@@ -14,19 +14,22 @@ get_header();
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'custom_justin_shaw' ); ?></h1>
+				<h1><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'custom_justin_shaw' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'custom_justin_shaw' ); ?></p>
-
-					<?php
+				<p class="error-content"><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'custom_justin_shaw' ); ?></p>
+        <div class="error-search">
+          <?php
 					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
+          ?>
+        </div>
+        <div class="error-posts">
+          <?php 
+          the_widget( 'WP_Widget_Recent_Posts' );
+          ?>
+        </div>
+					<div class="error-categories">
 						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'custom_justin_shaw' ); ?></h2>
 						<ul>
 							<?php
@@ -43,13 +46,16 @@ get_header();
 						</ul>
 					</div><!-- .widget -->
 
-					<?php
-					/* translators: %1$s: smiley */
-					$custom_justin_shaw_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'custom_justin_shaw' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$custom_justin_shaw_archive_content" );
+          <div class="error-archives">
+            <?php
+            /* translators: %1$s: smiley */
+            $custom_justin_shaw_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'custom_justin_shaw' ), convert_smilies( ':)' ) ) . '</p>';
+            the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$custom_justin_shaw_archive_content" );
 
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+            the_widget( 'WP_Widget_Tag_Cloud' );
+            ?>
+          </div>
+					
 
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
